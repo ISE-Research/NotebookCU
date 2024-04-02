07d341dc473bcbdb77f6f42537c606487d85de41
masihbr <masihbr@gmail.com>
2024-04-03 00:48:10 +0330

    Add train_model command

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import logging
 from enum import Enum
 from pathlib import Path
-from typing import Optional
+from typing import Literal, Optional
 
 import typer
 from typing_extensions import Annotated
 
 import config
+from classification_data import DataSelector
+from classifiers import (
+    CatBoostClassifierCustom,
+    DecisionTreeClassifierCustom,
+    RandomTreeClassifierCustom,
+    XGBoostClassifierCustom,
+)
 from logger import init_logger
 from notebook_metrics import aggregate_notebook_metrics
-from process_cell_metrics import (run_code_metrics_extraction,
-                                  run_markdown_metrics_extraction)
+from process_cell_metrics import run_code_metrics_extraction, run_markdown_metrics_extraction
 
 logger = logging.getLogger(__name__)
 app = typer.Typer(no_args_is_help=True)
@@ -19,6 +25,20 @@ app = typer.Typer(no_args_is_help=True)
 class FileType(str, Enum):
     markdown = "markdown"
     code = "code"
+
+class ModelType(str, Enum):
+    cat_boost = "cat_boost"
+    xgb_boost = "xgb_boost"
+    decision_tree = "decision_tree"
+    random_forest = "random_forest"
+
+    def get_classifier_class(self):
+        return {
+            ModelType.cat_boost: CatBoostClassifierCustom,
+            ModelType.xgb_boost: XGBoostClassifierCustom,
+            ModelType.decision_tree: DecisionTreeClassifierCustom,
+            ModelType.random_forest: RandomTreeClassifierCustom,
+        }.get(self.value)
 
 
 @app.command()
@@ -68,21 +88,21 @@ def extract_metrics(
 @app.command()
 def aggregate_metrics(
     code_metrics_df_file_path: Annotated[
-        Path, 
+        Path,
         typer.Argument(),
-        ] = Path(config.CODE_METRICS_DF_FILE_PATH),
+    ] = Path(config.CODE_METRICS_DF_FILE_PATH),
     markdown_metrics_df_file_path: Annotated[
-        Path, 
+        Path,
         typer.Argument(),
-        ] = Path(config.MARKDOWN_METRICS_DF_FILE_PATH),
-    notebook_metrics_df_file_path:Annotated[
-        Path, 
+    ] = Path(config.MARKDOWN_METRICS_DF_FILE_PATH),
+    notebook_metrics_df_file_path: Annotated[
+        Path,
         typer.Argument(),
-        ] = Path(config.NOTEBOOK_METRICS_DF_FILE_PATH),
+    ] = Path(config.NOTEBOOK_METRICS_DF_FILE_PATH),
     user_pt_metrics_df_file_path: Annotated[
-        Optional[Path], 
+        Optional[Path],
         typer.Argument(),
-        ] = None,
+    ] = None,
 ):
     """
     Aggregate code metrics and markdown metrics to get the notebook metrics dataframe.
@@ -93,8 +113,93 @@ def aggregate_metrics(
         code_metrics_df_file_path=str(code_metrics_df_file_path.resolve()),
         markdown_metrics_df_file_path=str(markdown_metrics_df_file_path.resolve()),
         notebook_metrics_df_file_path=str(notebook_metrics_df_file_path.resolve()),
-        user_pt_metrics_df_file_path=str(user_pt_metrics_df_file_path.resolve()) if user_pt_metrics_df_file_path else None
+        user_pt_metrics_df_file_path=(
+            str(user_pt_metrics_df_file_path.resolve()) if user_pt_metrics_df_file_path else None
+        ),
     )
+
+
+def validate_metrics_filters_key(value: str) -> str:
+    if value not in DataSelector.NOTEBOOK_METRICS_FILTERS.keys():
+        raise typer.BadParameter(f"valid options: {', '.join(DataSelector.NOTEBOOK_METRICS_FILTERS.keys())}")
+    return value
+
+
+def validate_scores_filters_key(value: str) -> str:
+    if value not in DataSelector.NOTEBOOK_SCORES_FILTERS.keys():
+        raise typer.BadParameter(f"valid options: {', '.join(DataSelector.NOTEBOOK_SCORES_FILTERS.keys())}")
+    return value
+
+
+@app.command()
+def train_model(
+    model: Annotated[
+        ModelType,
+        typer.Option("--model", "-m"),  # model
+    ] = ModelType.cat_boost,
+    notebook_metrics_df_file_path: Annotated[
+        Path,
+        typer.Option("--notebook-metrics-df-file-path", "-f"),  # features
+    ] = Path(config.NOTEBOOK_METRICS_DF_FILE_PATH),
+    notebook_scores_df_file_path: Annotated[
+        Path,
+        typer.Option("--notebook-scores-df-file-path", "-s"),  # scores
+    ] = Path(config.NOTEBOOK_SCORES_DF_FILE_PATH),
+    model_file_path: Annotated[
+        Path,
+        typer.Option("--folder-path-to-store-model", "-d"),  # destination folder
+    ] = Path(config.DEFAULT_MODEL_FILE_PATH),
+    selected_score: Annotated[
+        str,
+        typer.Option("--selected-score", "-ss"),  # selected score
+    ] = "combined_score",
+    notebook_metrics_filters_key: Annotated[
+        str,
+        typer.Option(
+            "--metrics-filters-key",
+            "-ffk",  # filter features key
+            callback=validate_metrics_filters_key,
+            help=f"Predefined key to filter notebook metrics based "
+            f"(valid options: {', '.join(DataSelector.NOTEBOOK_METRICS_FILTERS.keys())})",
+        ),
+    ] = "default",
+    notebook_scores_filters_key: Annotated[
+        str,
+        typer.Option(
+            "--scores-filters-key",
+            "-fsk",  # filter scores key
+            callback=validate_scores_filters_key,
+            help=f"Predefined key to filter notebook metrics based "
+            f"(valid options: {', '.join(DataSelector.NOTEBOOK_SCORES_FILTERS.keys())})",
+        ),
+    ] = "default",
+    notebook_metrics_filters: Annotated[
+        Optional[str],
+        typer.Option("--metrics-filters", "-ff"),  # filter features
+    ] = None,
+    notebook_scores_filters: Annotated[
+        Optional[str],
+        typer.Option("--scores-filters", "-fs"),  # filter scores
+    ] = None,
+):
+    model_class = model.get_classifier_class()
+    classifier = model_class.get_default_instance()
+
+    notebook_metrics_filters = DataSelector.NOTEBOOK_METRICS_FILTERS[notebook_metrics_filters_key]
+    notebook_scores_filters = DataSelector.NOTEBOOK_SCORES_FILTERS[notebook_scores_filters_key]
+
+    X_train, X_test, y_train, y_test = DataSelector(
+        notebook_metrics_df_file_path=str(notebook_metrics_df_file_path.resolve()),
+        notebook_scores_df_file_path=str(notebook_scores_df_file_path.resolve()),
+    ).get_train_test_split(
+        notebook_metrics_filters=notebook_metrics_filters,
+        notebook_scores_filters=notebook_scores_filters,
+        sort_by=selected_score,
+    )
+    classifier.train(X_train=X_train, y_train=y_train)
+    classifier.test(X_test=X_test, y_test=y_test)
+    classifier.save_model(str(model_file_path.resolve()))
+
 
 if __name__ == "__main__":
     init_logger()
