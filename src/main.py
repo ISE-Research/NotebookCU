b220e07d96329d71935fe5809b740e403a357186
masihbr <masihbr@gmail.com>
2024-04-12 11:18:33 +0330

    Add api to extract metrics of an uploaded file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-import os
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 from uuid import uuid4
 
 from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 from typing_extensions import Annotated
 
 import config
+from extract_metrics import extract_notebook_metrics_from_ipynb_file
 from logger import init_logger
 from model_store import ModelStore
 
@@ -21,14 +22,7 @@ async def get_models() -> Dict[str, Dict[str, Union[str, float, List[str]]]]:
 
 
 @app.post("/notebook/upload/")
-def upload_notebook(
-    file: Annotated[
-        UploadFile,
-        File(
-            description="Selected jupyter notebook.",
-        ),
-    ]
-):
+def upload_notebook(file: Annotated[UploadFile, File(description="Selected jupyter notebook.")]):
     filename = f"{uuid4().hex}-{file.filename}"
     notebooks_folder_path = Path(config.NOTEBOOKS_FOLDER_PATH)
     filepath = notebooks_folder_path / filename
@@ -38,9 +32,23 @@ def upload_notebook(
     return {"message": filename}
 
 
+class MetricsExtractionInfo(BaseModel):
+    notebook_filename: str
+    base_code_df_filename: Optional[str] = Path(config.CODE_DF_FILE_PATH).name
+    chunk_size: Optional[int] = config.CHUNK_SIZE
+
+
 @app.post("/notebook/metrics/")
-def extract_metrics():
-    pass
+def extract_metrics(info: MetricsExtractionInfo):
+    file_path = Path(config.NOTEBOOKS_FOLDER_PATH) / info.notebook_filename
+    base_code_df_file_path = Path(config.DATAFRAMES_FOLDER_PATH) / info.base_code_df_filename
+    extracted_notebook_metrics_df = extract_notebook_metrics_from_ipynb_file(
+        file_path=str(file_path.resolve()),
+        base_code_df_file_path=str(base_code_df_file_path.resolve()),
+        chunk_size=info.chunk_size,
+    )
+    extracted_notebook_metrics_df.drop(["kernel_id"], axis=1, inplace=True)
+    return {"message": extracted_notebook_metrics_df.iloc[[0]].to_dict(orient="index")[0]}
 
 
 @app.post("/notebook/predict/")
@@ -50,4 +58,5 @@ def predict():
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
